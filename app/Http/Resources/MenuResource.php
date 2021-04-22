e68f7bbaebda735592feceb8f685c62e92b52089
unknown <akosiyawin@gmail.com>
2021-04-22 20:41:31 +0800

    Save for heroku

--- app/Http/Resources/MenuResource.php
+++ app/Http/Resources/MenuResource.php
@@ -20,7 +20,8 @@ class MenuResource extends JsonResource
             'id' => $this->id,
             'category_id' => $this->category_id,
             'ingredients' => $this->ingredients,
-            'image_path' => env("APP_URL") . Storage::url("images/" . $this->image_path),
+            // 'image_path' => env("APP_URL") . Storage::url("images/" . $this->image_path),
+            'image_path' => Storage::url("images/" . $this->image_path),
             'price' => $this->price,
         ];
     }
